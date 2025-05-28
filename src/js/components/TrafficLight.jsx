8df5bfe41b979619efcbfb619b09da704643c0a1
Clay Beach <wcbeach30@gmail.com>
2025-05-28 23:33:28 +0000

    added glowing to the traffic lights when you click on them

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 
 //create your first component
 const Home = () => {
+  const [glowLight, setGlowLight] = useState(null);
+
   return (
     <div>
-        <div className="traffic-light-box">
-			<div className="traffic-light-pole"></div>
-          <div className="red"></div>
+      <div className="traffic-light-box">
+        <div className="traffic-light-pole"></div>
 
-          <div className="yellow"></div>
+        <div
+         className={`red ${glowLight === "red" ? "glow" : ""}`}
+         onClick={() => setGlowLight("red")}
+         ></div>
 
-          <div className="green"></div>
+        <div 
+        className={`yellow ${glowLight === "yellow" ? "glow" : ""}`}
+         onClick={() => setGlowLight("yellow")}></div>
+
+        <div 
+        className={`green ${glowLight === "green" ? "glow" : ""}`}
+         onClick={() => setGlowLight("green")}></div>
       </div>
     </div>
   );
