04ebb79c8ceae230278b49f4ee0784a8285a11d1
Clay Beach <wcbeach30@gmail.com>
2025-05-28 21:25:39 +0000

    got the traffic box with three colored circles centered in the middle, no functionality yet

---
+++ src/js/components/TrafficLight.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+//include images into your bundle
+
+//create your first component
+const Home = () => {
+  return (
+    <div>
+      <div className="traffic-light-stem">
+        <div className="traffic-light-box">
+          <div className="red"></div>
+
+          <div className="yellow"></div>
+
+          <div className="green"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
